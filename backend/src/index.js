ddeb03fc3d5e30c6008d662cc2c8d8930ea68be5
Sachi <202452318@iiitvadodara.ac.in>
2026-02-07 10:13:37 +0530

    imageupload

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/authroute.js";
+import messageRoutes from "./routes/messageRoute.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT " + PORT);
